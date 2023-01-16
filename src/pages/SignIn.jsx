36c7f9f92d6982d69b7a296c8f273b79d89a9fec
PavelSP <SmirnovPavel1988@yandex.ru>
2023-01-16 21:47:26 +0300

    10lesTestFail

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { isAuth } from '../store/profile/actions'
+import { signIn } from "../service/firebase";
+// import { SignUp } from "./SignUp";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export function SignIn() {
+    const [inputs, setInputs] = useState({ email: '', password: '' })
+    const [err, setErr] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate()
+    const dispach=useDispatch()
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setErr('')
+        setLoading(true)
+
+        try {
+            await signIn(inputs.email, inputs.password);
+            dispach(isAuth(true))
+            navigate('/chats')
+        } catch (err) {
+            setErr(err.message)
+            console.log(err)
+            setInputs({email: '', password: ''})
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    return (
+        <>
+            <div>Sign In</div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                Email:
+                <input type='email' name="email" value={inputs.email}
+                    onChange={(e) => setInputs((prev)=>({ ...prev, [e.target.name]: e.target.value }))} />
+                </label>
+                <label>
+                Password:
+                <input type='text' name="password" value={inputs.password}
+                    onChange={(e) => setInputs((prev)=>({ ...prev, [e.target.name]: e.target.value }))} />
+                </label>
+                <button>Sign In</button>
+                
+            </form>
+            {loading && <Box sx={{ display: 'flex', justifyContent:'center' }}><CircularProgress />
+                </Box>}
+            {err && <p>{err}</p>}        
+            </>
+    )
+}
